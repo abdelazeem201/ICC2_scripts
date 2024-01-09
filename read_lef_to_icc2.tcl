e5cffcbdd34206335099300424bb4326aebacde8
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2024-01-09 16:47:20 +0300

    read_lef_to_icc2.tcl

--- read_lef_to_icc2.tcl
+++ read_lef_to_icc2.tcl
@@ -2,11 +2,16 @@
 set synospsy_tech "/data/proj/library/SAED32nm/tech/milkyway/saed32nm_1p9m_mw.v10.tf"
 
 ### create workspace
-create_workspace -technology $synospsy_tech -flow frame readndmlib
+create_workspace -technology $synospsy_tech -flow exploration readndmlib
 
+set_pvt_configuration -voltage {0.95 1.16} -temperatures {125 -40}
+read_dc [glob ./libs/DB/*.db]
 read_lef /data/proj/library/SAED32nm/lib/stdcell_hvt/lef/saed32nm_hvt_1p9m.lef
+
+group_libs
+process_workspaces
 
 check_workspace
 commit_workspace
 
-/data/proj/library/SAED32nm/lib/stdcell_hvt/ndm/saed32_hvt_std.ndm/
+
